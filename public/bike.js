78755562fb628d090b35fce927b827260c3ab17a
Vega22s <kusch4960@gmail.com>
2025-03-16 22:17:52 +0700

    Add files via upload

---
+++ public/bike.js
@@ -0,0 +1,120 @@
+// bike.js
+const bikes = [
+    {
+        id: "1234",
+        brand: "Branchi",
+        color: "Blue shades",
+        image: "img/Bianchi Duel Azzurra Blue.jpg",
+        status: "Available",
+        
+    },
+    {
+        id: "6789",
+        brand: "Cube",
+        color: "Grey shades",
+        image: "img/Cube Analog Flashgrey.jpg",
+        status: "Borrowed"
+    },
+    {
+        id: "5432",
+        brand: "Cube",
+        color: "Green shades",
+        image: "img/Cube Attention SLX Willowgreen.jpg",
+        status: "Pending"
+    },
+    {
+        id: "9876",
+        brand: "Cube",
+        color: "White shades",
+        image: "img/Cube Reaction C62 Cottonwhite.jpg",
+        status: "Disabled"
+    },
+    {
+        id: "5555",
+        brand: "Giant",
+        color: "Blue shades",
+        image: "img/Giant Escape Disc Metallic Blue.jpg",
+        status: "Available",
+        
+    },
+    {
+        id: "5558",
+        brand: "Giant",
+        color: "Mars Dust",
+        image: "img/Giant Talon 0 Mars Dust.jpg",
+        status: "Available",
+        
+    },
+    {
+        id: "1235",
+        brand: "Giant",
+        color: "Blue shades",
+        image: "img/Giant Talon 0 Ultra Navy.jpg",
+        status: "Available",
+        
+    },
+    {
+        id: "1236",
+        brand: "Giant",
+        color: "Grey shades",
+        image: "img/Giant Talon 1 Charcoal.jpg",
+        status: "Available",
+        
+    },
+    {
+        id: "1237",
+        brand: "Kona Bikes",
+        color: "White shades",
+        image: "img/Kona Bikes Fire Moutain White.jpg",
+        status: "Available",
+        
+    },
+    {
+        id: "1238",
+        brand: "Kona Bikes",
+        color: "Green shades",
+        image: "img/Kona Bikes Process Aqua Green.jpg",
+        status: "Available",
+        
+    },
+    {
+        id: "1239",
+        brand: "Liv",
+        color: "Starry Night",
+        image: "img/Liv Lurra 1 Starry Night.jpg",
+        status: "Available",
+        
+    },
+    {
+        id: "1240",
+        brand: "Liv",
+        color: "Milkyway",
+        image: "img/Liv Tempt 1 Milkyway.jpg",
+        status: "Available",
+        
+    },
+    {
+        id: "1241",
+        brand: "Ridgeback",
+        color: "Maroon",
+        image: "img/Ridgeback Open Frame Maroon Red.jpg",
+        status: "Available",
+        
+    },
+    {
+        id: "1242",
+        brand: "Trek",
+        color: "Viper Red",
+        image: "img/Trek FX 1 Viper Red.jpg",
+        status: "Available",
+        
+    },
+    {
+        id: "1243",
+        brand: "Yeti Cycles",
+        color: "Blue shades",
+        image: "img/Yeti Cycles SB115 Light Blue.jpg",
+        status: "Available",
+        
+    }
+];
